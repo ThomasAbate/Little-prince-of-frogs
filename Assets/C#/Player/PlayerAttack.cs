efb6c6dfaa80cdebb0bab6b4fdbc59b386ae6a75
ThAb <tabate@artfx.fr>
2022-11-21 17:52:33 +0100

    [feat] boss
    [add] goblin

--- Assets/C#/Player/PlayerAttack.cs
+++ Assets/C#/Player/PlayerAttack.cs
@@ -41,6 +41,7 @@ public class PlayerAttack : MonoBehaviour
             foreach (Collider2D enemy in hitEnemies)
             {
                 enemy.GetComponent<BasicEnemy>().TakeDamage(Damage);
+                enemy.GetComponent<BossHealth>().TakeDamage(Damage);
                 Debug.Log("SHEEEEE");
 
             }
@@ -59,6 +60,7 @@ public class PlayerAttack : MonoBehaviour
             foreach (Collider2D enemy in hitEnemies)
             {
                 enemy.GetComponent<BasicEnemy>().TakeDamage(HugeDamage);
+                enemy.GetComponent<BossHealth>().TakeDamage(HugeDamage);
                 Debug.Log("SHEEEEE");
             }
         }
